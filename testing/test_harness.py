ca39035f8b2cb76118c81086be8f8821cbb48fa7
Pedro Larios <pedro.larios@gmail.com>
2018-08-30 15:37:13 -0500

    add license tests

--- testing/test_harness.py
+++ testing/test_harness.py
@@ -8,39 +8,132 @@ __version__ = "1.0"
 import argparse
 import inspect
 import json
+import os
+import subprocess
+import sys
+import threading
+import time
 
+from monodrive import configuration, simulator
 import testing.tests as testsuite
 
 
 parser = argparse.ArgumentParser(description='monoDrive simulator automates test harness script')
 parser.add_argument('--sim-config', default='simulator.json')
 parser.add_argument('--vehicle-config', default='test.json')
-parser.add_argument('--sim-path', help='path to simulator executable')
 parser.add_argument('--exclude',
                     help='comma separated list of tests to exclude (tests not in list will be included)')
 parser.add_argument('--include',
                     help='comma separated list of tests to include (tests not in list will be excluded)')
+parser.add_argument('simpath', metavar='simpath', help='path to simulator executable')
+
+
+class AtomicBool:
+    def __init__(self, value=False):
+        self.lock = threading.Lock()
+        self.value = value
+
+    def get(self):
+        self.lock.acquire()
+        value = self.value
+        self.lock.release()
+        return value
+
+    def set(self, value):
+        self.lock.acquire()
+        self.value = value
+        self.lock.release()
 
 
 class TestHarness:
     def __init__(self, args):
         self.args = args
-
+        self.simulator_process_thread = None
+        self.simulator_running = AtomicBool(False)
+        self.simulator = simulator.Simulator(configuration.SimulatorConfiguration(self.args.sim_config))
 
     def get_all_tests(self):
         tests = []
         base = testsuite.BaseTest.mro()[0]
         for name, cls in inspect.getmembers(testsuite):
             if name is not 'BaseTest' and inspect.isclass(cls) and base in cls.mro():
-                tests.append(cls)
+                tests.append(cls(self))
         return tests
 
-    def launch_simulator(self):
-        pass
+    def run_tests(self):
+        tests = self.get_all_tests()
+        if self.args.include:
+            tests = filter(lambda t: t.id in self.args.include)
+        elif self.args.exclude:
+            tests = filter(lambda t: t.id not in self.args.exclude)
+
+        tests = sorted(tests, key=lambda t: t.id)
+
+        for test in tests:
+            print("starting test %s" % test.id)
+            test.run()
+            print("result: %s" % test.result)
+            print("========================================\n")
+
+    def get_simulator_path(self):
+        return self.args.simpath[0:self.args.simpath.rfind(os.sep)]
+
+    def get_test_data_path(self):
+        return os.path.join('.','tests','data')
+
+    def get_simulator(self):
+        return self.simulator
+
+    def start_simulator(self):
+        if self.simulator_running.get():
+            print('simulator is already running')
+            return
+
+        print('launching %s' % self.args.simpath)
+        self.simulator_running.set(True)
+        self.simulator_process_thread = threading.Thread(target=self.launch_simulator_)
+        self.simulator_process_thread.start()
+
+    def launch_simulator_(self):
+        #print(subprocess.run(self.args.simpath, stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+        self.simulator_process = subprocess.Popen(self.args.simpath, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        while True:
+            try:
+                self.simulator_process.wait(timeout=.5)
+
+                if self.simulator_process is None or self.simulator_process.returncode is not None:
+                    break
+            except subprocess.TimeoutExpired:
+                if not self.simulator_running.get():
+                    break
+
+        self.simulator_running.set(False)
+        print("simulator exited")
+
+    def stop_simulator(self):
+        if self.simulator_running.get():
+            if self.simulator_process is not None and self.simulator_process.returncode is None:
+                print("shutting down simulator")
+                if sys.platform == 'win32' or sys.platform == 'win64':
+                    os.system("taskkill /f /t /pid %s" % self.simulator_process.pid)
+                else:
+                    self.simulator_process.kill()
+                    self.simulator_process.wait()
+            else:
+                print("simulator isnt running")
+
+            self.simulator_process = None
+            self.simulator_process_thread = None
+            self.simulator_running.set(False)
+
+    def wait(self):
+        if self.simulator_process_thread is not None:
+            self.simulator_process_thread.join()
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-
-    sim_config = SimulatorConfiguration(args.sim_config)
-    vehicle_config = VehicleConfiguration(args.vehicle_config)
+    env = TestHarness(args)
+    env.run_tests()
+    #sim_config = SimulatorConfiguration(args.sim_config)
+    #vehicle_config = VehicleConfiguration(args.vehicle_config)
--- testing/tests/licensing.py
+++ testing/tests/licensing.py
@@ -1,19 +1,88 @@
-from . import BaseTest
+import json
+import os
+import time
 
-class LicenseTest(BaseTest):
-    def __init__(self):
-        super(LicenseTest, self).__init__('licensing')
+from . import BaseTest, TestResult
+from monodrive.networking import messaging
 
-    def run(self):
-        self.test_missing_license()
-        self.test_bad_license()
-        self.test_good_license()
+class GoodLicenseTest(BaseTest):
+    def __init__(self, env):
+        super(GoodLicenseTest, self).__init__('licensing.1', env)
 
-    def test_missing_license(self):
-        pass
+    def before_test(self):
+        license = open(os.path.join(self.env.get_test_data_path(), 'license.txt')).read()
+        print(json.loads(license)['subscription_id'])
 
-    def test_bad_license(self):
-        pass
+        license_path = os.path.join(self.env.get_simulator_path(), 'license.txt')
+        if os._exists(license_path):
+            os.rename(license_path, license_path+'.tmp') # rename existing license file
 
-    def test_good_license(self):
-        pass
+        f = open(os.path.join(self.env.get_simulator_path(), 'license.txt'), 'w') # save test license file
+        f.write(license)
+        f.close()
+
+    def after_test(self):
+        license_path = os.path.join(self.env.get_simulator_path(), 'license.txt')
+        os.remove(license_path) # remove test license.txt
+        if os._exists(license_path + '.tmp'):
+            os.rename(license_path + '.tmp', license_path) # rename old license file
+
+    def test(self):
+        self.env.start_simulator()
+        time.sleep(10)
+        assert self.env.simulator_running.get()
+
+        simulator = self.env.get_simulator()
+        result = simulator.request(messaging.Message(messaging.SIMULATOR_STATUS_UUID))
+        assert result is not None and result.data is not None
+
+        self.env.stop_simulator()
+        self.result = TestResult(True, result.to_json())
+
+
+class MissingLicenseTest(BaseTest):
+    def __init__(self, env):
+        super(MissingLicenseTest, self).__init__('licensing.2', env)
+
+    def before_test(self):
+        license_path = os.path.join(self.env.get_simulator_path(), 'license.txt')
+        if os._exists(license_path):
+            os.rename(license_path, license_path + '.tmp')  # rename old license file
+
+    def after_test(self):
+        license_path = os.path.join(self.env.get_simulator_path(), 'license.txt')
+        if os._exists(license_path + '.tmp'):
+            os.rename(license_path + '.tmp', license_path)  # rename old license file
+
+    def test(self):
+        self.env.start_simulator()
+        time.sleep(10)
+
+
+class BadLicenseTest(BaseTest):
+    def __init__(self, env):
+        super(BadLicenseTest, self).__init__('licensing.3', env)
+
+    def before_test(self):
+        license = open(os.path.join(self.env.get_test_data_path(), 'license.txt')).read()
+        print(json.loads(license)['subscription_id'])
+
+        license_path = os.path.join(self.env.get_simulator_path(), 'license.txt')
+        if os._exists(license_path):
+            os.rename(license_path, license_path+'.tmp') # rename existing license file
+
+        f = open(os.path.join(self.env.get_simulator_path(), 'license.txt'), 'w') # save test license file
+        lic = json.loads(license)
+        lic['data'] = lic['data'][1:] # remove char from license data
+        f.write(json.dumps(lic))
+        f.close()
+
+    def after_test(self):
+        license_path = os.path.join(self.env.get_simulator_path(), 'license.txt')
+        os.remove(license_path)  # remove test license.txt
+        if os._exists(license_path + '.tmp'):
+            os.rename(license_path + '.tmp', license_path)  # rename old license file
+
+    def test(self):
+        self.env.start_simulator()
+        time.sleep(10)
